24cdfb64dcbb5875d1f08bcf0b297ed53d3e3c30
vinit <vinitnagar56@gmail.com>
2024-09-30 18:33:04 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-[#9873FE] flex justify-center items-center ">
+        <div className="p-3 rounded w-1/4 flex flex-col gap-2 bg-white">
+          <div>
+
+          </div>
+          <div className="bg-red-500 flex items-center  justify-center text-xl font-bold h-20"> 
+            <h2 className="hand">Currency converter</h2>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
